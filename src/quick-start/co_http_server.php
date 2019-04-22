5bc07b878d2c19439e57f8e2afa9002bc4797de1
Yabo <386139859@qq.com>
2019-04-22 23:03:44 +0800

    add co http server

---
+++ src/quick-start/co_http_server.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once(dirname(__DIR__, 2) . '/vendor/autoload.php');
+
+use Dotenv\Dotenv;
+
+Swoole\Runtime::enableCoroutine();
+
+$dotenv = Dotenv::create(dirname(__DIR__, 2));
+$dotenv->load();
+
+$http = new Swoole\Coroutine\Http\Client('0.0.0.0', 9501);
+
+$http->on('request', function($request, $response){
+    $db = new Swoole\Coroutine\MySQL();
+    $server = [
+        'host' => getenv('MYSQL_HOST'),
+        'port' => getenv('MYSQL_PORT'),
+        'user' => getenv('MYSQL_USERNAME'),
+        'password' => getenv('MYSQL_PASSWORD'),
+        'database' => getenv('MYSQL_DATABASE'),
+        'charset' => 'utf8',
+        'timeout' => 2,
+    ];
+
+    $db->connect($server);
+    $result = $db->query('select * from user');
+    $response->end(json_encode($result, JSON_UNESCAPED_UNICODE));
+});
+
+$http->start();
