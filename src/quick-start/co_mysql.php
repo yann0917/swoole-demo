1935e1c719919a02f49368d4d1ec0c5c96ccaab1
Yabo <386139859@qq.com>
2019-04-22 22:39:23 +0800

    :bug:

---
+++ src/quick-start/co_mysql.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once(dirname(__DIR__, 2) . '/vendor/autoload.php');
+
+use Dotenv\Dotenv;
+
+Swoole\Runtime::enableCoroutine();
+
+$dotenv = Dotenv::create(dirname(__DIR__, 2));
+$dotenv->load();
+
+go(function () {
+    $db = new Swoole\Coroutine\MySQL();
+
+    $server = [
+        'host' => getenv('MYSQL_HOST'),
+        'port' => getenv('MYSQL_PORT'),
+        'user' => getenv('MYSQL_USERNAME'),
+        'password' => getenv('MYSQL_PASSWORD'),
+        'database' => getenv('MYSQL_DATABASE'),
+        'charset' => 'utf8',
+        'timeout' => 2,
+    ];
+
+    $db->connect($server);
+
+    $db->query('show tables');
+});
