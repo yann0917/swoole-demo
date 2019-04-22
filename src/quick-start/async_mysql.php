ba629b98bc426ead04bdfa0d90c1c30f6580e6e7
Yabo <386139859@qq.com>
2019-04-22 22:00:02 +0800

    add async_mysql

---
+++ src/quick-start/async_mysql.php
@@ -0,0 +1,24 @@
+<?php
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::create(dirname(__DIR__, 2));
+$dotenv->load();
+
+$db = new Swoole\Mysql;
+
+$server = [
+    'host' => getenv('MYSQL_HOST'),
+    'port' => getenv('MYSQL_PORT'),
+    'user' => getenv('MYSQL_USERNAME'),
+    'password' => getenv('MYSQL_PASSWORD'),
+    'database' => getenv('MYSQL_DATABASE'),
+    'charset' => 'utf8',
+    'timeout' => 2,
+];
+
+$db->connect($server, function ($db, $result) {
+    $db->query('show tables', function (Swoole\Mysql $db, $result) {
+        var_dump($result);
+        $db->close();
+    });
+});
