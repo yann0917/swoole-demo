f97782155b6d67f4a2a247f2d62361fe6899749b
Yabo <386139859@qq.com>
2019-08-16 10:20:24 +0800

    test

--- src/quick-start/http_server.php
+++ src/quick-start/http_server.php
@@ -4,11 +4,16 @@ $http = new \Swoole\Http\Server('0.0.0.0', 9501);
 
 $http->on('request', function ($request, $response) {
     // var_dump($request->get, $request->post);
-    var_dump($request->server);
+    // var_dump($request->server);
     if ($request->server['path_info'] == '/favicon.ico' || $request->server['request_uri'] == '/favicon.ico') {
         return $response->end();
     }
 
+    $opts = [
+        'http' => ['method' => 'GET','timeout'=>1],
+    ];
+    $context = stream_context_create($opts);
+    file_get_contents('http://www.test.com', false, $context);
     $response->header("Content-type", "text/html;charset=utf-8");
     $response->end("<h1>Hello Swoole " . rand(100, 999) . "</h1>");
 });
